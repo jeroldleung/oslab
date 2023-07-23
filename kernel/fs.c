bcba920a7b75e3d36207b6ee1faa57051582e071
trabemazusa <zihao.liang@qq.com>
2023-07-23 21:41:47 +0800

    finish lab fs

--- kernel/fs.c
+++ kernel/fs.c
@@ -416,6 +416,40 @@ bmap(struct inode *ip, uint bn)
     brelse(bp);
     return addr;
   }
+  bn -= NINDIRECT;
+
+  if(bn < NDBLINDIRECT){
+    // Load doubly-indirect block, allocating if necessary.
+    if((addr = ip->addrs[NDIRECT+1]) == 0){
+      addr = balloc(ip->dev);
+      if(addr == 0)
+        return 0;
+      ip->addrs[NDIRECT+1] = addr;
+    }
+
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    if((addr = a[bn / NINDIRECT]) == 0){
+      addr = balloc(ip->dev);
+      if(addr == 0)
+        return 0;
+      a[bn / NINDIRECT] = addr;
+      log_write(bp);
+    }
+    brelse(bp);
+
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    if((addr = a[bn % NINDIRECT]) == 0){
+      addr = balloc(ip->dev);
+      if(addr == 0)
+	return 0;
+      a[bn % NINDIRECT] = addr;
+      log_write(bp);
+    }
+    brelse(bp);
+    return addr;
+  }
 
   panic("bmap: out of range");
 }
@@ -425,9 +459,9 @@ bmap(struct inode *ip, uint bn)
 void
 itrunc(struct inode *ip)
 {
-  int i, j;
-  struct buf *bp;
-  uint *a;
+  int i, j, k;
+  struct buf *bp, *bp1;
+  uint *a, *b;
 
   for(i = 0; i < NDIRECT; i++){
     if(ip->addrs[i]){
@@ -446,6 +480,26 @@ itrunc(struct inode *ip)
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
+  }
+
+  if(ip->addrs[NDIRECT+1]){
+    bp = bread(ip->dev, ip->addrs[NDIRECT+1]);
+    a = (uint*)bp->data;
+    for(j = 0; j < NINDIRECT; j++){
+      if(a[j] == 0)
+	continue;
+      bp1 = bread(ip->dev, a[j]);
+      b = (uint*)bp1->data;
+      for(k = 0; k < NINDIRECT; k++){
+        if(b[k])
+          bfree(ip->dev, b[k]);
+      }
+      brelse(bp1);
+      bfree(ip->dev, a[j]);
+    }
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT+1]);
+    ip->addrs[NDIRECT+1] = 0;
   }
 
   ip->size = 0;
--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -304,7 +304,7 @@ create(char *path, short type, short major, short minor)
 uint64
 sys_open(void)
 {
-  char path[MAXPATH];
+  char path[MAXPATH], tpath[MAXPATH];
   int fd, omode;
   struct file *f;
   struct inode *ip;
@@ -365,6 +365,30 @@ sys_open(void)
   }
 
   iunlock(ip);
+
+  for(int i = 0; ip->type == T_SYMLINK; i++){
+    if(omode & O_NOFOLLOW)
+      break;
+    if(i >= 10){
+      end_op();
+      return -1;
+    }
+    ilock(ip);
+    if(readi(ip, 0, (uint64)tpath, 0, MAXPATH) != MAXPATH){
+      iunlock(ip);
+      end_op();
+      return -1;
+    }
+    iunlock(ip);
+    if((ip = namei(tpath)) == 0){
+      end_op();
+      return -1;
+    }
+    ilock(ip);
+    f->ip = ip;
+    iunlock(ip);
+  }
+
   end_op();
 
   return fd;
@@ -501,5 +525,33 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+
+  // if the link already exist, return an error
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0){
+    end_op();
+    return -1;
+  }
+
+  if((writei(ip, 0, (uint64)target, 0, MAXPATH)) != MAXPATH){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
   return 0;
 }
